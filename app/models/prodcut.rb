58290564f1aaf82d2807a90b3a0adda159106f1d
Stefanie <stefanieheinz0@gmail.com>
2021-05-04 18:26:12 -0600

    Created products

--- app/models/prodcut.rb
+++
@@ -1,2 +0,0 @@
-class Prodcut < ApplicationRecord
-end
--- db/migrate/20210504234602_create_products.rb
+++ db/migrate/20210504234602_create_products.rb
@@ -1,6 +1,6 @@
-class CreateProdcuts < ActiveRecord::Migration[6.1]
+class CreateProducts < ActiveRecord::Migration[6.1]
   def change
-    create_table :prodcuts do |t|
+    create_table :products do |t|
       t.string :name
       t.integer :price
       t.string :image_url
