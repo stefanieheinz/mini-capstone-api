f34ef2445cd9c3b3e376b09a6434e9cd4e266538
Stefanie <stefanieheinz0@gmail.com>
2021-05-04 18:37:46 -0600

    Added routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def all_products
+    product = Product.all
+    render json: product.as_json
+  end
+
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def second_product
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def third_product
+    product = Product.third
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/all_products", controller: "products", action: "all_products"
+  get "/first_product", controller: "products", action: "first_product"
+  get "/second_product", controller: "products", action: "second_product"
+  get "/third_product", controller: "products", action: "third_product"
 end
